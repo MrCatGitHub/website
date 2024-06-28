6d6715323d8255142d528373296b2719bb35c41b
Mr. Cat <123008056+MrCatGitHub@users.noreply.github.com>
2024-06-28 19:55:30 +0200

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -7,35 +7,35 @@ import { extname } from 'node:path';
 const hostname = '192.168.178.30';
 const port = 3000;
 
-    const server = createServer(async (req, res) => {
+const server = createServer(async (req, res) => {
     const url = req.url === '/' ? '/index.html' : req.url;
 
-        // Check if the requested URL is /test.html
-        if (url === '/test.html') {
-            res.statusCode = 403;
-            res.setHeader('Content-Type', 'text/plain');
-            res.end('403 Forbidden');
-            return;
-        }
+    if (url === '/test.html') {
+        res.statusCode = 423;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('423 Locked');
+        return;
+    }
 
-    const filePath = `./public${url}`;
+    const filePath = `.${url}`;
     const ext = extname(filePath);
 
-        let contentType = 'text/html';
-        if (ext === '.js') {
-            contentType = 'application/javascript';
-        } else if (ext === '.css') {
-            contentType = 'text/css';
-        } else if (ext === '.json') {
-            contentType = 'application/json';
-        } else if (ext === '.png') {
-            contentType = 'image/png';
-        } else if (ext === '.jpg') {
-            contentType = 'image/jpg';
-        } else if (ext === '.webp') {
-            contentType = 'image/webp'
-        } // Add more MIME types as needed
+    let contentType = 'text/html';
+    if (ext === '.js') {
+        contentType = 'application/javascript';
+    } else if (ext === '.css') {
+        contentType = 'text/css';
+    } else if (ext === '.json') {
+        contentType = 'application/json';
+    } else if (ext === '.png') {
+        contentType = 'image/png';
+    } else if (ext === '.jpg') {
+        contentType = 'image/jpg';
+    } else if (ext === '.webp') {
+        contentType = 'image/webp'
+    }
 
+    try {
         const data = await readFile(filePath);
 
         res.statusCode = 200;
@@ -43,14 +43,13 @@ const port = 3000;
         res.end(data);
     } catch (error) {
         try {
-            const data = await readFile('./404.html');
             res.statusCode = 404;
             res.setHeader('Content-Type', 'text/html');
-            res.end(data);
+            res.end('404 Not Found');
         } catch (err) {
             res.statusCode = 500;
             res.setHeader('Content-Type', 'text/plain');
-            res.end('500: Internal Server Error');
+            res.end('500 Internal Server Error');
         }
     }
 });
