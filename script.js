7aa0b65c7ed6d87b64d48852995ecea500dcb77b
Mr. Cat <123008056+MrCatGitHub@users.noreply.github.com>
2024-06-29 20:57:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,6 @@ var btn = document.createElement("button");
 btn.textContent = "Click Me!";
 btn.id = "myButton";
 document.body.appendChild(btn);
+function openPopup() {
+    window.open('http://192.168.178.30/', 'woo', 'width=400,height=300');
+}
