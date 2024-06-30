ebb45f8927b2254236a7923d0ed2354b12425b7b
Mr. Cat <123008056+MrCatGitHub@users.noreply.github.com>
2024-06-30 23:11:00 +0200

    trying something

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const img = document.getElementById('profileImage');
-    img.onerror = function () {
-        img.src = img.alt;
-    };
-});
