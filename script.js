20b463cfb449d02d4097523e4bb4a28167d06bff
Mr. Cat <123008056+MrCatGitHub@users.noreply.github.com>
2024-07-01 20:26:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const img = document.getElementById('profileImage');
-    img.onerror = function () {
-        img.src = img.alt;
-    };
+document.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded and parsed');
+
+    const images = document.querySelectorAll('img[data-image-id]');
+
+    images.forEach(image => {
+        const imageId = image.getAttribute('data-image-id');
+        const newImageSrc = `./img/${imageId}.png`;
+
+        // Create a new image element to check if the image exists
+        const img = new Image();
+        img.src = newImageSrc;
+
+        img.onload = function () {
+            // Image exists, set it as the source
+            image.src = newImageSrc;
+        };
+
+        img.onerror = function () {
+            // Image does not exist, fallback to default image
+            image.src = './img/404.png';
+        };
+    });
 });
